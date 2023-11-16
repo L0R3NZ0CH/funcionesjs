4a6b67fbf07d5de38964b2788eeaafaa6a93eac1
L0R3NZ0CH <rdzcha77@gmail.com>
2023-11-16 01:44:28 -0600

    second commit

--- app.js
+++ app.js
@@ -40,6 +40,53 @@ function multiplicar() {
     
     document.getElementById('respuesta').textContent = resultado;
 }
+
+let boton = document.querySelector('.botonClick');
+//evento del mouse
+                    // evento que va escuchar, funcion que se va a ejecutar
+boton.addEventListener("click",    function () {
+      console.log('Dimos click en el boton click');
+      boton.classList.toggle('verde');
+    })
+
+boton.addEventListener("mouseover", function () {
+    console.log('EStamos sobre el boton');
+})
+boton.addEventListener("mouseout", function () {
+    console.log('Fuera del boton');
+})
+
+//Evento en el teclado
+window.addEventListener("keydown", (e) => {
+    console.log('Pulso la tecla');
+    console.log(String.fromCharCode(e.keyCode));
+})
+window.addEventListener('keypress', (e) => {
+console.log('Pulso la tecla');})
+
+window.addEventListener("keyup", (e) => {
+    console.log('Deje de pulsar la tecla');});
+
+
+window.addEventListener('load', () => {
+    console.log('Termine de cargar la paginado');})
+
+
+let bagregar =document.querySelector('.botonAgregar');
+let textoInput = document.querySelector('.inputTexto');
+let textBox = document.querySelector('.textoBox');
+
+bagregar.addEventListener('click', function(){
+let textoIngresado = textoInput.value;
+
+    textBox.value += textoIngresado;
+    textoInput.value ='';
+
+
+});
+
+
+
 // suma(5,6);
 // function suma(a,b,c) {
 // return console.log(a,b,c);
@@ -59,5 +106,4 @@ function multiplicar() {
 
 // let holaArrow = (nombre) => console.log('Hola' + nombre + 'desde una arrow  ');
 // holaArrow('Chantal');
-    
 // }
